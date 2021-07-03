1511dbd4a626e7a8ced3b668e6b97ba7f9a586a8
j-moriarti <58417739+j-moriarti@users.noreply.github.com>
2021-07-03 18:33:42 +0000

    ...

--- Download-and-Process-Hosts.sh
+++ Download-and-Process-Hosts.sh
@@ -33,7 +33,7 @@ rm -f *.txt.raw
 touch mkb2091_whitelist_domains.txt.raw
 
 # Download raw sources using the lists in 'sources.conf' file (in parallel).
-curl -L -R -s --compressed --parallel --parallel-max 10 --parallel-immediate --connect-timeout 10 --retry 5 --retry-connrefused --retry-delay 5 -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36" -K "./sources.conf"
+curl -K "sources.conf" -L -R -s --compressed --parallel --parallel-max 10 --parallel-immediate --connect-timeout 10 --retry 5 --retry-connrefused --retry-delay 5 -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
 
 # Process some raw sources to be in correct format.
 sed -i 's/<.*$//;/^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$/!d' gameindustry_adobe-inc_hosts.txt.raw
